7370926e4901f70b30947ecc1422169a0c22703f
Philip <wowcxy2008@126.com>
2019-02-20 18:06:08 +0800

    Update .deploy.js

--- .deploy.js
+++ .deploy.js
@@ -2,16 +2,16 @@
  * 部署配置
  * @author Philip
  */
+
 module.exports = {
-  name: 'portal',
+  name: 'raddeana',
   dist: 'dist',
   type: 'web',
   build: 'build/build.js',
-  project: 'portal',
   ali_oss: {
     accessKeyId: 'LTAI2PBQSdfLOUme',
     accessKeySecret: 'uMuFXEuK06PGTEmHRiFCvoCNtgx8nb',
-    bucket: 'raddeana-portal',
+    bucket: 'raddeana',
     region: 'oss-cn-hangzhou'
   }
 }
